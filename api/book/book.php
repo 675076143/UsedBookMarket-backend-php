c8f467a4297bbdd733334913549cdcd372e03a74
675076143 <36965465+675076143@users.noreply.github.com>
2020-01-21 18:34:02 +0800

    Book Module(GET)

--- api/book/book.php
+++ api/book/book.php
@@ -8,14 +8,28 @@ $json_data = json_decode($json_raw,true);
 //创建SQL连接
 header('content-type:application/json;charset=utf-8');
 $link = initMySqlConnector();
-$categoryID = $json_data["categoryID"];
-//查询语句
-$where = isset($categoryID)?"where categoryID=$categoryID":"";
-$sql = "SELECT * from book $where";
-if($res = fetchAll($link,$sql)){
-    $result = array("code"=>'200',"message"=>"success","data"=>$res);
-    exit(json_encode($result));
-}else{
-    $result = array("code"=>'400',"message"=>"未知错误，请联系系统管理员","data"=>null);
-    exit(json_encode($result));
+$categoryID = isset($_GET["categoryID"])?$_GET["categoryID"]:null;
+$bookID = isset($_GET["bookID"])?$_GET["bookID"]:null;
+if(strtoupper($_SERVER['REQUEST_METHOD'])=='GET'){
+    $where = "";
+    //查询语句
+    if($categoryID){//如果有分类ID代表获取该分类下的所有书本
+        $where = " where categoryID=$categoryID";
+    }elseif($bookID){//如果有bookID,则代表获取单个书本详情信息
+        $where =" where bookID=$bookID";
+    }else{//否则获取所有数据
+        $where = "";
+    }
+    $sql = "SELECT * from book $where";
+    if($res = fetchAll($link,$sql)){
+        //如果只有一条数据，就不返回数组了
+        if(count($res) === 1){
+            $res = $res[0];
+        }
+        $result = array("code"=>'200',"message"=>"success","data"=>$res);
+        exit(json_encode($result));
+    }else{
+        $result = array("code"=>'400',"message"=>"failed","data"=>null);
+        exit(json_encode($result));
+    }
 }
