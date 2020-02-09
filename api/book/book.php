426fd2fbbe6b56c9599031dd653751f340c6d9a9
675076143 <36965465+675076143@users.noreply.github.com>
2020-02-09 21:58:18 +0800

    publish,modify,delete book

--- api/book/book.php
+++ api/book/book.php
@@ -9,15 +9,22 @@ $json_data = json_decode($json_raw,true);
 $link = initMySqlConnector();
 $categoryID = isset($_GET["categoryID"])?$_GET["categoryID"]:null;
 $bookID = isset($_GET["bookID"])?$_GET["bookID"]:null;
+$userID = isset($_GET["userID"])?$_GET["userID"]:null;
+
+$bookName = isset($json_data["image"])?$json_data["bookName"]:null;
+$image = isset($json_data["image"])?$json_data["image"]:null;
+$bookDesc = isset($json_data["bookDesc"])?$json_data["bookDesc"]:null;
+$price = isset($json_data["price"])?$json_data["price"]:null;
+
 if(strtoupper($_SERVER['REQUEST_METHOD'])=='GET'){
     $where = "";
     //Query statement
     if($categoryID){//If there is a classification ID, all books under the classification will be obtained
         $where = " where categoryID=$categoryID";
-    }elseif($bookID){//If there is a bookid, it means to get individual book details
+    }elseif($bookID){//If there is a bookID, it means to get individual book details
         $where =" where bookID=$bookID";
-    }else{//Otherwise, get all data
-        $where = "";
+    }elseif($userID){//If there is a userID, it means to get published of this user
+        $where = "where userID=$userID";
     }
     $sql = "SELECT * from book $where";
     if($res = fetchAll($link,$sql)){
@@ -29,6 +36,48 @@ if(strtoupper($_SERVER['REQUEST_METHOD'])=='GET'){
         exit(json_encode($result));
     }else{
         $result = array("code"=>'400',"message"=>"failed","data"=>null);
+        exit(json_encode($result));
+    }
+}elseif (strtoupper($_SERVER['REQUEST_METHOD'])=='POST'){// add book
+    $userID = $json_data["userID"];
+    $categoryID = $json_data["categoryID"];
+    $sql = "INSERT INTO book 
+            (`bookName`,`bookDesc`,`price`,`status`,`image`,`categoryID`,`userID`) 
+            VALUES
+            ('$bookName','$bookDesc',$price,0,'$image','$categoryID',$userID)";
+    if($res = query($link,$sql)){
+        $result = array("code"=>'200',"message"=>"add selling book successful","data"=>null);
+        exit(json_encode($result));
+    }else{
+        $result = array("code"=>'400',"message"=>"Unknown error, please contact system administrator","data"=>null);
+        exit(json_encode($result));
+    }
+}elseif (strtoupper($_SERVER['REQUEST_METHOD'])=='PUT'){// modify book
+    $categoryID = $json_data["categoryID"];
+    $bookID = $json_data["bookID"];
+    $sql = "UPDATE book
+            SET 
+            `bookName` = '$bookName',
+            `bookDesc` = '$bookDesc',
+            `price` = '$price',
+            `image` = '$image',
+            `categoryID` = '$categoryID'
+            WHERE `bookID` = '$bookID'";
+    if($res = query($link,$sql)){
+        $result = array("code"=>'200',"message"=>"modify successful","data"=>null);
+        exit(json_encode($result));
+    }else{
+        $result = array("code"=>'400',"message"=>"Unknown error, please contact system administrator","data"=>null);
+        exit(json_encode($result));
+    }
+}elseif (strtoupper($_SERVER['REQUEST_METHOD'])=='DELETE'){// delete book
+    $bookID = $json_data["bookID"];
+    $sql = "DELETE FROM book WHERE bookID='$bookID'";
+    if($res = query($link,$sql)){
+        $result = array("code"=>'200',"message"=>"delete successful","data"=>null);
+        exit(json_encode($result));
+    }else{
+        $result = array("code"=>'400',"message"=>"Unknown error, please contact system administrator","data"=>null);
         exit(json_encode($result));
     }
 }
--- headers.php
+++ headers.php
@@ -2,6 +2,6 @@
 //Allow other domain names to access
 header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Credentials", "true");
-header('Access-Control-Allow-Methods:GET,POST,OPTIONS,PUT');
+header('Access-Control-Allow-Methods:GET,POST,OPTIONS,PUT,DELETE');
 header('Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept, Authorization');
 header('content-type:application/json;charset=utf-8');
